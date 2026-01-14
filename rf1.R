cfa844b84a4c4275a3b469ba360dbae60cad92a9
theo <theo.constantin01@etu.umontpellier.fr>
2026-01-14 11:37:47 +0100

    ajout prédiction et modification dossier de sortie

--- rf1.R
+++ rf1.R
@@ -5,54 +5,48 @@
 library(randomForest)
 library(tools)
 
-# 1. RÉCUPÉRATION DE L'ARGUMENT (ex: kebab_matrix.RData)
 args <- commandArgs(trailingOnly = TRUE)
 
-if (length(args) == 0) {
-  stop("Erreur : Donnez le nom du fichier. Exemple : Rscript rf.R kebab_matrix.RData", call. = FALSE)
-}
-# 
 nom_fichier_entree <- args[1]
 
-# 2. CONSTRUCTION DES CHEMINS
 # Chemin vers le dossier source
 dossier_source <- "RData"
 chemin_entree  <- file.path(dossier_source, nom_fichier_entree)
 
 # Chemin vers le dossier destination
-dossier_models <- "models"
+dossier_models <- file.path("models", "rf_70-30")
 if (!dir.exists(dossier_models)) {
-  dir.create(dossier_models)
+  dir.create(dossier_models, recursive = TRUE)
 }
 
-# Génération du nom de sortie : models/rf1_nom_du_lieu.rds
+# Génération du nom de sortie : models/rf_70-30/rf1_nom_du_lieu.rds
 nom_lieu       <- file_path_sans_ext(nom_fichier_entree)
 chemin_sortie  <- file.path(dossier_models, paste0("rf1_", nom_lieu, ".rds"))
 
-# 3. CHARGEMENT
-if (!file.exists(chemin_entree)) {
-  stop(paste("Erreur : Le fichier est introuvable dans :", chemin_entree), call. = FALSE)
-}
 
-cat("Chargement de :", chemin_entree, "...\n")
 load(chemin_entree)
 df <- df_final 
 
-# 4. PRÉPARATION (70/30)
 set.seed(123)
-idx   <- sample(1:nrow(df), 0.7 * nrow(df))
+idx   <- sample(1:nrow(df), 0.7 * nrow(df)) # 70% pour l'entrainement, 30% pour le test
 train <- df[idx, ]
 test  <- df[-idx, ]
 
-# 5. ENTRAÎNEMENT
-cat("Entraînement du modèle [rf1] pour", nom_lieu, "...\n")
 m_rf <- randomForest(hauteur ~ ., data = train, ntree = 500, importance = TRUE)
 
-# 6. SAUVEGARDE
-saveRDS(m_rf, chemin_sortie)
-cat("--> Modèle sauvegardé dans :", chemin_sortie, "\n")
 
-# 7. PERFORMANCE RAPIDE
+saveRDS(m_rf, chemin_sortie)
+
+# prédiction
+exemple_ligne  <- test[1, ] 
+valeur_reelle  <- exemple_ligne$hauteur
+valeur_predite <- predict(m_rf, newdata = exemple_ligne)
+
+cat("Prédiction : \n")
+cat("Valeur Réelle  :", valeur_reelle, "\n")
+cat("Valeur Prédite :", valeur_predite, "\n")
+cat("Valeur absolue :", abs(valeur_predite - valeur_reelle), "\n")
+
 pred_rf <- predict(m_rf, test)
 r2      <- 1 - sum((test$hauteur - pred_rf)^2) / sum((test$hauteur - mean(test$hauteur))^2)
-cat("Précision (R2) :", round(r2, 4), "\n")
+cat("R2 :", round(r2, 4), "\n")
